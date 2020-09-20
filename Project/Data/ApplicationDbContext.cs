2a145db60689bbda7eeac7174efba2778d2869ff
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-09-20 03:49:02 +0300

    Configured relations and created migration.

---
+++ Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+namespace Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<UserActivity> UserActivities { get; set; }
+        public DbSet<UserMessage> UserMessages { get; set; }
+        public DbSet<UserPost> UserPosts { get; set; }
+        public DbSet<UserTicket> UserTickets { get; set; }
+        public DbSet<UserVPS> UserVPSs { get; set; }
+        public DbSet<VPS> VPSs { get; set; }
+        public DbSet<VPSActivity> VPSActivities { get; set; }
+        public DbSet<VPSState> VPSStates { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => base.OnConfiguring(optionsBuilder);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.HasKey(user => user.Id);
+
+                entity.HasMany(user => user.VPSs)
+                .WithOne(vps => vps.User)
+                .HasForeignKey(userVps => userVps.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(user => user.Posts)
+                .WithOne(post => post.User)
+                .HasForeignKey(userPost => userPost.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(user => user.Tickets)
+                .WithOne(ticket => ticket.User)
+                .HasForeignKey(userTicket => userTicket.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(user => user.Messages)
+                .WithOne(message => message.User);
+
+                entity.HasMany(user => user.Activities)
+                .WithOne(activity => activity.User)
+                .HasForeignKey(userActivity => userActivity.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<VPS>(entity =>
+            {
+                entity.HasMany(vps => vps.Activities)
+                .WithOne(activity => activity.VPS)
+                .HasForeignKey(vpsActivity => vpsActivity.VPSId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(vps => vps.States)
+                .WithOne(state => state.VPS)
+                .HasForeignKey(vpsState => vpsState.VPSId)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Post>()
+            .HasMany(post => post.Answers)
+            .WithOne(answer => answer.ParentPost)
+            .HasForeignKey(answer => answer.ParentId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<UserVPS>()
+            .HasKey(userVps => new { userVps.UserId, userVps.VPSId });
+
+            modelBuilder.Entity<UserPost>()
+            .HasKey(userPost => new { userPost.UserId, userPost.PostId });
+
+            modelBuilder.Entity<UserTicket>()
+            .HasKey(userTicket => new { userTicket.UserId, userTicket.TicketId });
+
+            modelBuilder.Entity<UserMessage>()
+            .HasKey(userMessage => new { userMessage.UserId, userMessage.MessageId });
+
+            modelBuilder.Entity<UserActivity>()
+            .HasKey(userActivity => new { userActivity.UserId, userActivity.ActivityId });
+
+            modelBuilder.Entity<VPSActivity>()
+            .HasKey(vpsActivity => new { vpsActivity.VPSId, vpsActivity.ActivityId });
+
+            modelBuilder.Entity<VPSState>()
+            .HasKey(vpsState => new { vpsState.VPSId, vpsState.StateId });
+        }
+    }
+}
---
+++ Project/Data/Migrations/20200920003329_AddModels.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project.Data.Migrations
+{
+    public partial class AddModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Announcements",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Content = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Announcements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(maxLength: 200, nullable: false),
+                    URL = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Subject = table.Column<string>(maxLength: 50, nullable: false),
+                    Message = table.Column<string>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    ParentId = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Posts_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Posts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Subject = table.Column<string>(maxLength: 50, nullable: false),
+                    Message = table.Column<string>(nullable: false),
+                    Priority = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VPSs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Plan = table.Column<int>(nullable: false),
+                    Location = table.Column<int>(nullable: false),
+                    IP = table.Column<string>(maxLength: 15, nullable: false),
+                    Cores = table.Column<byte>(nullable: false),
+                    RAM = table.Column<byte>(nullable: false),
+                    SSD = table.Column<byte>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VPSs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VPSs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMessages",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    MessageId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMessages", x => new { x.UserId, x.MessageId });
+                    table.ForeignKey(
+                        name: "FK_UserMessages_Messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Messages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMessages_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPosts",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    PostId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPosts", x => new { x.UserId, x.PostId });
+                    table.ForeignKey(
+                        name: "FK_UserPosts_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserPosts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTickets",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    TicketId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTickets", x => new { x.UserId, x.TicketId });
+                    table.ForeignKey(
+                        name: "FK_UserTickets_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserTickets_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Message = table.Column<string>(maxLength: 100, nullable: false),
+                    URL = table.Column<string>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    VPSId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activities_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Activities_VPSs_VPSId",
+                        column: x => x.VPSId,
+                        principalTable: "VPSs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    CPU = table.Column<float>(nullable: false),
+                    RAM = table.Column<float>(nullable: false),
+                    VPSId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_States_VPSs_VPSId",
+                        column: x => x.VPSId,
+                        principalTable: "VPSs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserVPSs",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    VPSId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserVPSs", x => new { x.UserId, x.VPSId });
+                    table.ForeignKey(
+                        name: "FK_UserVPSs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserVPSs_VPSs_VPSId",
+                        column: x => x.VPSId,
+                        principalTable: "VPSs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserActivities",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    ActivityId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserActivities", x => new { x.UserId, x.ActivityId });
+                    table.ForeignKey(
+                        name: "FK_UserActivities_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserActivities_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VPSActivities",
+                columns: table => new
+                {
+                    VPSId = table.Column<string>(nullable: false),
+                    ActivityId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VPSActivities", x => new { x.VPSId, x.ActivityId });
+                    table.ForeignKey(
+                        name: "FK_VPSActivities_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VPSActivities_VPSs_VPSId",
+                        column: x => x.VPSId,
+                        principalTable: "VPSs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VPSStates",
+                columns: table => new
+                {
+                    VPSId = table.Column<string>(nullable: false),
+                    StateId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VPSStates", x => new { x.VPSId, x.StateId });
+                    table.ForeignKey(
+                        name: "FK_VPSStates_States_StateId",
+                        column: x => x.StateId,
+                        principalTable: "States",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VPSStates_VPSs_VPSId",
+                        column: x => x.VPSId,
+                        principalTable: "VPSs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_UserId",
+                table: "Activities",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_VPSId",
+                table: "Activities",
+                column: "VPSId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserId",
+                table: "Messages",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ParentId",
+                table: "Posts",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_States_VPSId",
+                table: "States",
+                column: "VPSId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActivities_ActivityId",
+                table: "UserActivities",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMessages_MessageId",
+                table: "UserMessages",
+                column: "MessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPosts_PostId",
+                table: "UserPosts",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTickets_TicketId",
+                table: "UserTickets",
+                column: "TicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserVPSs_VPSId",
+                table: "UserVPSs",
+                column: "VPSId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VPSActivities_ActivityId",
+                table: "VPSActivities",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VPSs_UserId",
+                table: "VPSs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VPSStates_StateId",
+                table: "VPSStates",
+                column: "StateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Announcements");
+
+            migrationBuilder.DropTable(
+                name: "UserActivities");
+
+            migrationBuilder.DropTable(
+                name: "UserMessages");
+
+            migrationBuilder.DropTable(
+                name: "UserPosts");
+
+            migrationBuilder.DropTable(
+                name: "UserTickets");
+
+            migrationBuilder.DropTable(
+                name: "UserVPSs");
+
+            migrationBuilder.DropTable(
+                name: "VPSActivities");
+
+            migrationBuilder.DropTable(
+                name: "VPSStates");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "States");
+
+            migrationBuilder.DropTable(
+                name: "VPSs");
+        }
+    }
+}
