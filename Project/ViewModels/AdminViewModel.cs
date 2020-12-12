962de5815e057f5699a2098ce16905941bd38115
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-12-12 18:19:39 +0200

    Added admin view model.

---
+++ Project/ViewModels/AdminViewModel.cs
@@ -0,0 +1,14 @@
+namespace Project.ViewModels {
+	public class AdminViewModel {
+		public int UsersCount { get; set; }
+		public int AnnouncementsCount { get; set; }
+		public int VPSsCount { get; set; }
+		public int OrdersCount { get; set; }
+		public int PromoCodesCount { get; set; }
+		public int TicketsCount { get; set; }
+		public double DailyIncome { get; set; }
+		public double MonthlyIncome { get; set; }
+		public double YearlyIncome { get; set; }
+		public double TotalIncome { get; set; }
+	}
+}
