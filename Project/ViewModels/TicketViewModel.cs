e8dcf6f665de2296715871c4a9eb406214c18f27
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-12-15 03:47:05 +0200

    Created ticket view models.

---
+++ Project/ViewModels/TicketViewModel.cs
@@ -0,0 +1,13 @@
+using Project.Enums;
+using Project.Models;
+
+namespace Project.ViewModels {
+	public class TicketViewModel {
+		public string Id { get; set; }
+		public string Subject { get; set; }
+		public string Message { get; set; }
+		public Priority Priority { get; set; }
+		public string AnswerId { get; set; }
+		public Message Answer { get; set; }
+	}
+}
---
+++ Project/ViewModels/TicketsViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Project.ViewModels {
+	public class TicketsViewModel : ListViewModel {
+		public TicketsViewModel() => this.Tickets = new List<TicketViewModel>();
+
+		public List<TicketViewModel> Tickets { get; set; }
+		public int LowPriorityCount { get; set; }
+		public int MediumPriorityCount { get; set; }
+		public int HighPriorityCount { get; set; }
+		public int AnsweredCount { get; set; }
+	}
+}
