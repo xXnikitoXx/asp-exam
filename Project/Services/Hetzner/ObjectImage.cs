6a21ab185261cba3c3968dde08a3c8086a633064
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-12-24 19:18:24 +0200

    Added missing classes from Hetzner Cloud API.

---
+++ Project/Services/Hetzner/ObjectImage.cs
@@ -0,0 +1,44 @@
+using lkcode.hetznercloudapi.Objects.Universal;
+using System;
+using System.Collections.Generic;
+
+namespace lkcode.hetznercloudapi.Objects.Image.Get
+{
+	public class Response
+	{
+		public List<Image.Universal.Image> images { get; set; }
+		public Meta meta { get; set; }
+	}
+}
+
+namespace lkcode.hetznercloudapi.Objects.Image.Universal
+{
+	public class Protection
+	{
+		public bool delete { get; set; }
+	}
+
+	public class Labels
+	{
+	}
+
+	public class Image
+	{
+		public long id { get; set; }
+		public string type { get; set; }
+		public string status { get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+		public double? image_size { get; set; }
+		public double? disk_size { get; set; }
+		public DateTime created { get; set; }
+		public object created_from { get; set; }
+		public int? bound_to { get; set; }
+		public string os_flavor { get; set; }
+		public string os_version { get; set; }
+		public bool rapid_deploy { get; set; }
+		public Protection protection { get; set; }
+		public string deprecated { get; set; }
+		public Labels labels { get; set; }
+	}
+}
